712bcdd60592503e77a49461ae30131f9f702d91
Jack Reynolds <132878618+jr4488@users.noreply.github.com>
2026-08-08 21:27:28 -0700

    Wire canonical public shell into the existing application

--- src/components/site/PublicShell.tsx
+++ src/components/site/PublicShell.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 
-import styles from './PublicSite.module.css'
+import legacyStyles from './PublicSite.module.css'
+import { PublicEffects } from './PublicEffects'
 import { SiteFooter } from './SiteFooter'
 import { SiteHeader } from './SiteHeader'
 
@@ -10,12 +11,11 @@ interface PublicShellProps {
 
 export function PublicShell({ children }: PublicShellProps) {
   return (
-    <div className={styles.page}>
-      <a className={styles.skipLink} href="#main-content">
-        Skip to main content
-      </a>
+    <div className={`${legacyStyles.page} wc-site`}>
+      <a className="wc-skip-link" href="#main-content">Skip to main content</a>
       <SiteHeader />
-      <main className={styles.main} id="main-content">
+      <PublicEffects />
+      <main className={`${legacyStyles.main} wc-main`} id="main-content">
         {children}
       </main>
       <SiteFooter />
