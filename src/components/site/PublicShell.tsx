48464604125bbfe2665b34927e40b44484551947
Jack Reynolds <132878618+jr4488@users.noreply.github.com>
2026-08-08 21:29:13 -0700

    Isolate canonical public styling from the application theme

--- src/components/site/PublicShell.tsx
+++ src/components/site/PublicShell.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react'
 
-import legacyStyles from './PublicSite.module.css'
 import { PublicEffects } from './PublicEffects'
 import { SiteFooter } from './SiteFooter'
 import { SiteHeader } from './SiteHeader'
@@ -11,11 +10,11 @@ interface PublicShellProps {
 
 export function PublicShell({ children }: PublicShellProps) {
   return (
-    <div className={`${legacyStyles.page} wc-site`}>
+    <div className="wc-site">
       <a className="wc-skip-link" href="#main-content">Skip to main content</a>
       <SiteHeader />
       <PublicEffects />
-      <main className={`${legacyStyles.main} wc-main`} id="main-content">
+      <main className="wc-main" id="main-content">
         {children}
       </main>
       <SiteFooter />
